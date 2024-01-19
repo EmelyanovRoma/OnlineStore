d7b173880739d37b22f9036ba9b3eb9a190d0f1b
Romanter456@gmail.com <Romanter456@gmail.com>
2024-01-19 19:40:04 +0700

    feature (index.php): добавлен файл index.php

---
+++ OnlineStore/src/php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Добро пожаловать! | Online Store</title>
+</head>
+<body>
+    <div class="wrapper">
+    <header id="open" class="header">
+            <div id="close" class="header__container">
+                <div class="header__logo">
+                    <a href="../php/index.php" class="logo__link">
+                        <img src="../../img/logo.png" alt="Логотип">
+                    </a>
+                </div>
+            </div>
+        </header>
+        <div class="start_page">
+            <div class="start_page__container">
+                <h1 class="start_page__title">Добро пожаловать!</h1>
+                <a href="../pages/login.html" class="login__link link">Войти</a>
+                <span class="text">или</span>
+                <a href="../pages/registration.html" class="registration__link link">Зарегистрироваться</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
