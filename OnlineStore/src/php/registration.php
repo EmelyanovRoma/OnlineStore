663159ee84d18091482eb6d79b4e12745761e238
Romanter456@gmail.com <Romanter456@gmail.com>
2024-01-19 21:45:13 +0700

    feature (registration.php): добавлена возможность регистрации на сайте

--- OnlineStore/src/php/registration.php
+++ OnlineStore/src/php/registration.php
@@ -1,28 +1,30 @@
 <?php
 require_once('data_base.php');
-$login = $_POST['login'];
-$pass = $_POST['pass'];
-$repeat_pass = $_POST['repeat_password'];
-$email = $_POST['email'];
 
-
-if (empty($login) || empty($pass) || empty($repeat_pass) || empty($email)){
-	echo "Заполните все поля!";
-} else
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
-	if($pass != $repeat_pass)
-	{
-		echo "Пароли не совпадают!";
-	} else 
-	{
-		$sql = "INSERT INTO `users` (login, password, email) VALUES ('$login', '$pass', '$email')";
-	}
-	if ($connection -> query($sql) === TRUE)
-	{
-	 echo "Успешная регистрация!";
-	} else 
-	{
-	 echo "Ошибка: " . $connection->error;
-	}
+    $password = $_POST['password'];
+    $repeat_password = $_POST['repeat_password'];
+    $email = $_POST['email'];
+    
+    if (empty($password) || empty($repeat_password) || empty($email)){
+        echo "Заполните все поля!";
+    } else
+    {
+        if($password != $repeat_password)
+        {
+            echo "Пароли не совпадают!";
+        } else 
+        {
+            $sql = "INSERT INTO `User` (password, email) VALUES ('$password', '$email')";
+        }
+        if ($connection -> query($sql) === TRUE)
+        {
+         echo "Успешная регистрация!";
+        } else 
+        {
+         echo "Ошибка: " . $connection->error;
+        }
+    }
 }
 ?>
